5218887fce45c89d755d6f7ba23fed7b8f348ec6
Matias Lizana <matias.lizana.garcia@gmail.com>
2019-12-17 08:42:42 +0100

    Stable dialog and breath looping at the same time

--- breath.cpp
+++ breath.cpp
@@ -10,6 +10,7 @@ Matias Lizana García 2019
 
 FMOD_DSP_PARAMETER_DESC* paramdesc[] =
 {
+	&dialog,
 	&breath,
 	&frequency,
 	&distance,
@@ -25,7 +26,7 @@ FMOD_DSP_DESCRIPTION FMOD_Breathalog_Desc =
 	CreateCallback,
 	ReleaseCallback,
 	0,
-	ReadCallback, 
+	ReadCallback,
 	0,
 	0,
 	INTRF_NUM_PARAMETERS,
@@ -49,7 +50,8 @@ extern "C"
 {
 	F_EXPORT FMOD_DSP_DESCRIPTION* F_CALL FMODGetDSPDescription()
 	{
-		FMOD_DSP_INIT_PARAMDESC_DATA(breath, "wave data", "", "wave data", FMOD_DSP_PARAMETER_DATA_TYPE_USER);
+		FMOD_DSP_INIT_PARAMDESC_DATA(dialog, "Dialog", "", "Dialog file", FMOD_DSP_PARAMETER_DATA_TYPE_USER);
+		FMOD_DSP_INIT_PARAMDESC_DATA(breath, "Breath", "", "Breath file", FMOD_DSP_PARAMETER_DATA_TYPE_USER);
 		FMOD_DSP_INIT_PARAMDESC_FLOAT(frequency, "Frequency", "s", "frequency", 0, 2, 0);
 		FMOD_DSP_INIT_PARAMDESC_FLOAT(distance, "Distance", "s", "distance", 0, 2, 0);
 		return &FMOD_Breathalog_Desc;
@@ -64,57 +66,48 @@ FMOD_RESULT F_CALLBACK CreateCallback(FMOD_DSP_STATE* dsp_state)
 	if (!data)
 		return FMOD_ERR_MEMORY;
 	dsp_state->plugindata = data;
-	
+
+	data->threshold = 0.01f;
+	data->dialog_find_window = 10000;
+
 	return FMOD_OK;
 }
 
 FMOD_RESULT F_CALLBACK ReadCallback(FMOD_DSP_STATE* dsp_state, float* inbuffer, float* outbuffer, unsigned int length, int inchannels, int* outchannels)
 {
 	dsp_data* data = (dsp_data*)dsp_state->plugindata;
-
-	for (unsigned int samp = 0; samp < length; samp++) {
-		outbuffer[samp] = data->breath[data->breath_index];
-		if (data->breath_index++ > data->breath_samples)
+	for (unsigned int s = 0; s < length; s++) {
+		outbuffer[s] = data->dialog[data->dialog_index]*0.5f + data->breath[data->breath_index]*0.5f;
+		if (data->dialog_index++ == data->dialog_samples) {
+			data->dialog_index = 0;
+		}
+		if (data->breath_index++ == data->breath_samples) {
 			data->breath_index = 0;
+		}
 	}
 
-	//TODO: Com buidar els marcadors cada cop que processem noves dades del buffer?
 	/*
-	//We assume its mono
-	dsp_data* data = (dsp_data*)dsp_state->plugindata;
-
-	//We store the thresholded audio, to know where to discriminate values for a dialog
-	for (unsigned int samp = 0; samp < length; samp++)
-		data->processed_buffer[samp] = abs(data->buffer[samp]) > data->threshold ? 1 : 0;
-
-	//2. Find the markers for the dialog in/out using a window
-	for (unsigned int samp = 1; samp < length; samp++) {
-		bool found = 0;
-		//Comes from dialog and now it's silence
-		if (data->processed_buffer[samp] == 0 && data->processed_buffer[samp - 1] == 1) {
-			//Uses the blank window to fill gaps that are still dialog
-			for (unsigned int t = 0; t < data->blank_window; t++) {
-				//If found some dialog, quit
-				if (data->processed_buffer[samp + t] == 1)
-					data->processed_buffer[samp] = 1;
-				found = 1;
-				break;
-			}
-			//If there's no dialog on this window, it's a mark out
-			if (found == 0) {
-				data->markers_out[data->markers_out_index] = samp;
-				data->markers_out_index++;
-			}
+	for (unsigned int s = 0; s < length; s++) {
+		
+		// Dialog output
+		outbuffer[s] = data->dialog[data->dialog_index];
+		if (data->dialog_index++ == data->dialog_samples) {
+			data->dialog_index = 0;
+			data->breath_index = 0;
+			min_index = markers_in.begin();
+			mout_index = markers_out.begin();
 		}
-		//Comes from silence and now it's dialog
-		else if(data->processed_buffer[samp] == 1 && data->processed_buffer[samp-1] == 0) {
-
+		// Check if there's some breath to mix
+		if (data->dialog_index >= *mout_index && data->breath_index < data->breath_samples) {
+			outbuffer[s] = data->breath[data->breath_index];
+			data->breath_index++;
+		// Breath is over
+		} else if (data->breath_index == data->breath_samples) {
+			mout_index++;
+			data->breath_index = 0;
 		}
 	}
 	*/
-	   
-	//outbuffer[(samp * inchannels) + chan] = inbuffer[(samp * inchannels) + chan] * voice_shatter + noise;
-
 	return FMOD_OK;
 }
 
@@ -146,17 +139,69 @@ FMOD_RESULT F_CALLBACK ReleaseCallback(FMOD_DSP_STATE* dsp_state)
 
 FMOD_RESULT F_CALLBACK SetParamDataCallback(FMOD_DSP_STATE* dsp_state, int index, void* value, unsigned int length)
 {
-	if (index == 0)
-	{
-		dsp_data* data = (dsp_data*)dsp_state->plugindata;
+	dsp_data* data = (dsp_data*)dsp_state->plugindata;
 
-		data->breath = (float*) malloc(length);
+	if (index == 0) {
+
+		//0. Load dialog file
+		data->dialog = (float*)malloc(length);
+		if (!data->dialog)
+			return FMOD_ERR_MEMORY;
+		data->dialog = (float*)value;
+		data->dialog_index = 0;	// Initialize index pointer
+		data->dialog_samples = length / 4; // Number of samples are floats
+
+		data->dialog_thr = (float*)malloc(length);
+		if (!data->dialog_thr)
+			return FMOD_ERR_MEMORY;
+
+		//1. We store the thresholded audio, to know where to discriminate values for a dialog
+		for (unsigned int s = 0; s < data->dialog_samples; s++)
+			data->dialog_thr[s] = fabs(data->dialog[s]) > data->threshold ? 1 : 0;
+
+		//2. Find the markers for the dialog in/out using a window
+		for (unsigned int s = 1; s < data->dialog_samples - data->dialog_find_window; s++) {
+			bool found = 0;
+			//Comes from dialog and now it's silence
+			if (data->dialog_thr[s] == 0 && data->dialog_thr[s - 1] == 1) {
+				//Uses the blank window to fill gaps that are still dialog
+				for (unsigned int t = 0; t < data->dialog_find_window; t++) {
+					//If found some dialog, quit
+					if (data->dialog_thr[s + t] == 1) {
+						data->dialog_thr[s] = 1;
+						found = 1;
+						break;
+					}
+				}
+				//If there's no dialog on this window, it's a mark out
+				if (found == 0)
+					markers_out.push_back(s);
+			}
+			//Comes from silence and now it's dialog
+			else if (data->dialog_thr[s] == 1 && data->dialog_thr[s - 1] == 0) {
+				int inPivot = s - data->breath_samples;
+				//Check if the breath fits at the begining of the audio
+				if (inPivot >= 0)	//TODO: Check for overlapping in and out breaths
+					markers_in.push_back(inPivot);
+			}
+		}
+
+		min_index = markers_in.begin();
+		mout_index = markers_out.begin();
+
+		return FMOD_OK;
+	}
+
+	// TODO: Waiting to load more types of breath
+	if (index == 1)
+	{
+		data->breath = (float*)malloc(length);
 		if (!data->breath)
 			return FMOD_ERR_MEMORY;
 
-		data->breath = (float*) value;  
+		data->breath = (float*)value;
 		data->breath_index = 0;	// Initialize index pointer
-		data->breath_samples = length / 4;
+		data->breath_samples = length / 4; // Number of samples are floats
 
 		return FMOD_OK;
 	}
@@ -167,16 +212,10 @@ FMOD_RESULT F_CALLBACK SetParamDataCallback(FMOD_DSP_STATE* dsp_state, int index
 FMOD_RESULT F_CALLBACK SetParamFloatCallback(FMOD_DSP_STATE* dsp_state, int index, float value)
 {
 	dsp_data* mydata = (dsp_data*)dsp_state->plugindata;
-	/*
-	if (index == 1)
-		mydata->voice_shatter = value;
-	else if (index == 2)
-		mydata->noise_volume = value;
-	else if (index == 3)
-		mydata->noise_shatter = value;
-	else
-		return FMOD_ERR_INVALID_PARAM;
-		*/
+
+	if (index == 2)
+		mydata->freq = value;
+
 	return FMOD_OK;
 }
 
@@ -184,16 +223,12 @@ FMOD_RESULT F_CALLBACK GetParamFloatCallback(FMOD_DSP_STATE* dsp_state, int inde
 {
 	dsp_data* mydata = (dsp_data*)dsp_state->plugindata;
 
-	/*
-	if (index == 1)
-		* value = mydata->voice_shatter;
-	else if (index == 2)
-		* value = mydata->noise_volume;
-	else if (index == 3)
-		* value = mydata->noise_shatter;
-	else
-		return FMOD_ERR_INVALID_PARAM;
+	if (index == 2)
+		* value = mydata->freq;
 
-		*/
 	return FMOD_OK;
+}
+
+void CalculatePreprocessBuffer() {
+
 }
--- breath.hpp
+++ breath.hpp
@@ -1,13 +1,15 @@
 #include "fmod.hpp"
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
+#include <list>
 
 /* DLL Generation */
 extern "C" {
 	F_EXPORT FMOD_DSP_DESCRIPTION* F_CALL FMODGetDSPDescription();
 }
 
-#define INTRF_NUM_PARAMETERS 3
+#define INTRF_NUM_PARAMETERS 4
 
 FMOD_RESULT F_CALLBACK ReadCallback(FMOD_DSP_STATE* dsp_state, float* inbuffer, float* outbuffer, unsigned int length, int inchannels, int* outchannels);
 FMOD_RESULT F_CALLBACK CreateCallback(FMOD_DSP_STATE* dsp_state);
@@ -16,37 +18,36 @@ FMOD_RESULT F_CALLBACK SetParamDataCallback(FMOD_DSP_STATE* dsp_state, int index
 FMOD_RESULT F_CALLBACK SetParamFloatCallback(FMOD_DSP_STATE* dsp_state, int index, float value);
 FMOD_RESULT F_CALLBACK GetParamFloatCallback(FMOD_DSP_STATE* dsp_state, int index, float* value, char* valstr);
 
+void CalculatePreprocessBuffer();
+
+static FMOD_DSP_PARAMETER_DESC dialog;
 static FMOD_DSP_PARAMETER_DESC breath;
 static FMOD_DSP_PARAMETER_DESC frequency;
 static FMOD_DSP_PARAMETER_DESC distance;
 
-typedef struct
+typedef struct dsp
 {
-	//Data storage
-	float* buffer;
-	float* processed_buffer;
-
 	//Params
-	float* breath; // Breath audio
-	unsigned int breath_index; // index to read breath audio
-	unsigned int breath_samples; // length of the breath audio
+	float* dialog;							// Dialog audio
+	float* breath;							// Breath audio TODO: Fill with an array of breaths (in, out, mid)
+	float freq;								// Frequency of breathing mid
+	float distance;							// Distance between another breaths with mid
 
-	// TODO: Fill with an array of breaths (in, out, mid)
-	float freq;	// Frequency of breathing mid
-	float distance; //Distance between another breaths with mid
-
-	float breath_mid_fade_time;
-	float blank_window;
-	float threshold;
-	unsigned int* markers_out;
-	unsigned int markers_out_index;
-	unsigned int* markers_in;
-	unsigned int markers_in_index;
-
-	//Struct params
-	int   length_samples;
-	int   channels;
+	//Variables
+	float* dialog_thr;						// Buffer to store dialog thresholded
+	unsigned int dialog_index;				// Index to read dialog audio
+	unsigned int dialog_samples;			// Length of the dialog audio
+	unsigned int breath_index;				// Index to read breath audio
+	unsigned int breath_samples;			// Length of the breath audio
+	float breath_mid_fade_time;				// Fade time to mix dialog with breath
+	float dialog_find_window;				// Window time to calculate real dialog
+	float threshold;						// Threshold to calculate dialog volume
 } dsp_data;
+
+std::list<unsigned int> markers_out;			// List of breath out markers
+std::list<unsigned int> markers_in;				// List of breath in markers
+std::list<unsigned int>::iterator mout_index;	// Index to track which marker out to search for
+std::list<unsigned int>::iterator min_index;	// Index to track which marker in to search for
 
 extern FMOD_DSP_PARAMETER_DESC* paramdesc[INTRF_NUM_PARAMETERS];
 
